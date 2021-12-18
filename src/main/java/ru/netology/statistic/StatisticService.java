419c8cfc2470183362da894e561ea3022cb54729
Mamontov Aleksey <amamont868617@gmail.com>
2021-12-18 14:09:12 +0300

    update3

--- src/main/java/ru/netology/statistic/StatisticService.java
+++ src/main/java/ru/netology/statistic/StatisticService.java
@@ -7,14 +7,7 @@ public class StatisticService {
      * @param incomes - array of incomes
      * @return - index of first max value
      */
-    public long findMax(long[] incomes) {
-        long current_max_index = 0;
-        long current_max = incomes[0];
-        for (long income : incomes)
-            if (current_max < income)
-                current_max = income;
-        return current_max;
-    }
+
 
     public long findMin(long[] incomes) {
         long current_min_index = 0;
@@ -33,4 +26,10 @@ public class StatisticService {
 
         return sum;
     }
+    public long mean(long[] incomes) {
+       return sum(incomes) / incomes.length;
+
+        }
+
+
 }
--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -6,17 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticServiceTest {
 
-    @Test
-    void findMax() {
-        StatisticService service = new StatisticService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
 
     @Test
     void findMin() {
@@ -38,6 +28,17 @@ class StatisticServiceTest {
         long expected = 85;
 
         long actual = service.sum(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void mean() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 7;
+
+        long actual = service.mean(incomesInBillions);
 
         assertEquals(expected, actual);
     }
